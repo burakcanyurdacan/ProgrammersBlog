b32e943a68dcdb9e433dcebad0b7f34fbeebb671
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2026-03-10 17:44:50 +0300

    Data katmanına UnitOfWork pattern'i eklendi. Buradaki amaç, birden fazla işlemin tek seferde DB'ye gönderilmesidir. - IUnitOfWork interface eklendi ve repository nesneleri tanımlandı. - UnitOfWork class'ı eklendi.

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } //_unitOfWork.Users.AddAsync();
+        Task<int> SaveAsync();
+    }
+}
