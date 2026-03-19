b52c9ab1ccf14d7c262b9805c3a1a5e54591a912
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2026-03-19 17:04:38 +0300

    Result işlemleri için Result yapısı oluşturuldu. İşlemlerin olumlu ya da olumsuz olarak kullanıcıya bildirilmesi için gerekli yapı hazırlandı.

---
+++ ProgrammersBlog.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,40 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            ResultMessage = message;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            ResultMessage = message;
+            Data = data;
+            Exception = exception;
+        }
+
+        public T Data { get; }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string ResultMessage { get; }
+
+        public Exception Exception { get; }
+    }
+}
