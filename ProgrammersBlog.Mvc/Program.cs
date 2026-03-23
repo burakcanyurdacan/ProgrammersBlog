9bff8eae8f07b316d86f5012bdc34822172fead1
Burak Can Yurdacan <burakcanyurdacan@outlook.com>
2026-03-23 17:41:10 +0300

    AutoMapper kullanılarak DTO ların bağlantısı sağlandı. IServiceCollection pattern kullanılarak Program.cs üzerinde tüm servislerin çağrılması sağlandı. Bu sayede MVC katmanı servisleri kullanabilecek.

--- ProgrammersBlog.Mvc/Program.cs
+++ ProgrammersBlog.Mvc/Program.cs
@@ -1,4 +1,8 @@
+using ProgrammersBlog.Services.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.LoadMyServices();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
